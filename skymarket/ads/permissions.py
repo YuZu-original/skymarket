f686c46229a2380de18c4e9992dfb5963d162ad8
YuZu <yurkazubkov@yandex.ru>
2022-11-20 17:06:32 +0300

    upd

---
+++ skymarket/ads/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import BasePermission
+from users.models import UserRoles
+
+
+class IsAdminPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == UserRoles.ADMIN
+
+
+class IsOwnerPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user and request.user and obj.author == request.user
